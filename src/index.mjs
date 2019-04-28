51a32af3e99a461f145ed9abeb2f25c0bb80f462
Gerald Michelant <gerald.michelant@gmail.com>
2019-04-28 08:57:06 -0400

    initial queries

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import ApolloServer from './utils/ase.cjs';
 import cors from 'cors';
+import {users, me } from './utils/users.mjs'
 
 const app = express();
 app.use(cors());
@@ -8,19 +9,26 @@ app.use(cors());
 const schema = `
   type Query {
     me: User
+    users: [User!]
+    user(id: ID!): User
   }
   type User {
+    id: ID!
     username: String!
   }
   `;
 const resolvers = {
   Query: {
     me: () => {
-      return {
-        username: 'Gerald Michelant',
-      };
+      return me
     },
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
   },
+}
 };
 
 const server = new ApolloServer({
---
+++ src/utils/users.mjs
@@ -0,0 +1,14 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+const me = users[1];
+
+export { users, me}
