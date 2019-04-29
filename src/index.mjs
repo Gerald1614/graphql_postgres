b7e1b68e63cb39b42063dc876741aa0e42b16762
Gerald Michelant <gerald.michelant@gmail.com>
2019-04-29 09:32:59 -0400

    delete mutation

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import ApolloServer from './utils/ase.cjs';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4.js';
 import { users, messages } from './utils/users.mjs'
 
 const app = express();
@@ -14,6 +15,10 @@ const schema = `
     messages: [Message!]!
     message(id: ID!): Message!
   }
+  type Mutation {
+    createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
+  }
   type User {
     id: ID!
     username: String!
@@ -24,6 +29,7 @@ const schema = `
     text: String!
     user: User!
   }
+
   `;
 const resolvers = {
   Query: {
@@ -42,6 +48,29 @@ const resolvers = {
     message: (parent, { id }) => {
       return messages[id];
     },
+  },
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message = {
+        id,
+        text,
+        userId: me.id,
+      };
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+      return message;
+    },
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+      delete messages[id];
+      return true;
+    },
+
   },
   User: {
     messages: user => {
